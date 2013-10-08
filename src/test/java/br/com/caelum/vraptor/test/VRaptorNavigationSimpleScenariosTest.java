660c0597d8d008253cea4852f2ae20ea01fe8363
Francisco Sokol <chico.sokol@gmail.com>
2013-10-08 16:27:29 -0300

    removing main

--- src/test/java/br/com/caelum/vraptor/test/VRaptorNavigationSimpleScenariosTest.java
+++ src/test/java/br/com/caelum/vraptor/test/VRaptorNavigationSimpleScenariosTest.java
@@ -2,54 +2,11 @@ package br.com.caelum.vraptor.test;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
-import java.net.URLClassLoader;
-
-import javax.naming.NamingException;
-import javax.servlet.jsp.JspFactory;
-
-import org.apache.jasper.JspC;
-import org.apache.jasper.runtime.HttpJspBase;
-import org.apache.tomcat.InstanceManager;
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockServletConfig;
-import org.springframework.mock.web.MockServletContext;
 
 import br.com.caelum.vraptor.test.models.Task;
 
 public class VRaptorNavigationSimpleScenariosTest extends VRaptorIntegration {
-
-	private static final class InstanceManagerImplementation implements InstanceManager {
-		@Override
-		public Object newInstance(String arg0, ClassLoader arg1) throws IllegalAccessException, InvocationTargetException,
-				NamingException, InstantiationException, ClassNotFoundException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public void newInstance(Object arg0) throws IllegalAccessException, InvocationTargetException, NamingException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public Object newInstance(String arg0) throws IllegalAccessException, InvocationTargetException, NamingException,
-				InstantiationException, ClassNotFoundException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public void destroyInstance(Object arg0) throws IllegalAccessException, InvocationTargetException {
-			// TODO Auto-generated method stub
-			
-		}
-	}
 
 	@Test
 	public void shouldExecuteDefaultForward() throws Exception {
@@ -99,30 +56,4 @@ public class VRaptorNavigationSimpleScenariosTest extends VRaptorIntegration {
 		assertEquals("Hello world from a jsp", html);
 	}
 	
-	public static void main(String[] args) throws Exception {
-		JspC jspC = new JspC();		
-		jspC.setUriroot(".");
-		jspC.setJspFiles("test.jsp");
-		jspC.setOutputDir("jsp-compilation");
-		jspC.setCompile(true);
-		jspC.execute();
-		File compilationDir = new File("jsp-compilation");
-		
-		URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] {compilationDir.toURI().toURL()});
-		Class<?> cls = Class.forName("org.apache.jsp.test_jsp", true, classLoader);
-		HttpJspBase instance = (HttpJspBase) cls.newInstance();
-		MockServletConfig servletConfig = new MockServletConfig(new MockServletContext());
-		servletConfig.getServletContext().setAttribute(InstanceManager.class.getName(), new InstanceManagerImplementation());
-		instance.init(servletConfig);
-		JspFactory.setDefaultFactory(new org.apache.jasper.runtime.JspFactoryImpl());
-		instance._jspInit();
-		
-		MockHttpServletResponse response = new MockHttpServletResponse();
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		request.setAttribute("title","funciona por favor");
-		request.setAttribute("list",new String[]{"a,b,c"});
-		instance._jspService(request,response);
-		System.out.println(response.getContentAsString().trim());
-	}
-
 }
