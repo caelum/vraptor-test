d4d09335c1670d7987c432525527975347404b4a
Francisco Sokol <chico.sokol@gmail.com>
2013-11-07 17:52:27 -0200

    fixing jsp compilation

---
+++ src/test/java/br/com/caelum/vraptor/test/JspTestProducer.java
@@ -0,0 +1,27 @@
+package br.com.caelum.vraptor.test;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
+
+import org.jboss.weld.manager.BeanManagerImpl;
+
+import br.com.caelum.vraptor.test.jspsupport.JspResolver;
+
+@ApplicationScoped
+@Alternative
+@Priority(value=Interceptor.Priority.LIBRARY_BEFORE + 100)
+public class JspTestProducer {
+
+	@Inject
+	private BeanManagerImpl beanManagerImpl;
+
+	@Produces
+	@ApplicationScoped
+	public JspResolver create(){
+		return new JspResolver("src/test/resources/WebContent/",beanManagerImpl);
+	}
+}
